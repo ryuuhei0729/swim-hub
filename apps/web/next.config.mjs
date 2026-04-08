417e004e45361b4b35beabe8d001640155b1ae6e
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2026-04-09 02:16:19 +0900

    微修正

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/{path}*",
         headers: [
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
